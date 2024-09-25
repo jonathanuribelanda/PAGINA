779f4d6aebab4356f88a9de3eac9ae5794872d3d
jonathanuribelanda <jona_uribe@hotmail.com>
2024-09-25 16:11:29 -0600

    Add files via upload

---
+++ conócenos/about-us.js
@@ -0,0 +1,306 @@
+const VALUES_CONTAINER = document.getElementById('values-container');
+const TEAMWORK_CONTAIENER = document.getElementById('value-teamwork');
+const SERVICE_CONTAIENER = document.getElementById('value-service');
+const EMPATHY_CONTAIENER = document.getElementById('value-empathy');
+const ETHIC_CONTAIENER = document.getElementById('value-ethic');
+const EFFICIENCY_CONTAIENER = document.getElementById('value-efficiency');
+
+/*  Se agregan escuchadores de eventos a todas las secciones de la 
+    página para que se reinicien los botones cada que se da click
+    fuera de su contenedor. */
+const MISION = document.getElementById('mission');
+MISION.addEventListener('click', ReplaceButtons);
+const VISION = document.getElementById('vison');
+VISION.addEventListener('click', ReplaceButtons);
+const HISTORY = document.getElementById('history');
+HISTORY.addEventListener('click', ReplaceButtons);
+const STAFF = document.getElementById('staff');
+STAFF.addEventListener('click', ReplaceButtons);
+
+const teamwork_button = document.getElementById('button-teamwork');
+teamwork_button.addEventListener('click', ShowValueTeamwork);
+const service_button = document.getElementById('button-service');
+service_button.addEventListener('click', ShowValueService);
+const empathy_button = document.getElementById('button-empathy');
+empathy_button.addEventListener('click', ShowValueEmpathy);
+const ethic_button = document.getElementById('button-ethic');
+ethic_button.addEventListener('click', ShowValueEthic);
+const efficiency_button = document.getElementById('button-efficiency');
+efficiency_button.addEventListener('click', ShowValueEfficiency);
+/*  Se declaran las variables que contienen los abjetos HTML y sus contenidos
+    así como sus clases y rutas.     */
+let teamwork = document.createElement('article');
+teamwork.className = 'identity-values-value';
+teamwork.id = 'value';
+let imagen_teamwork = document.createElement('img');
+imagen_teamwork.src = './assets/icons/teamwork-icon.svg'
+imagen_teamwork.className = 'identity-values-value-img';
+let titulo_teamwork = document.createElement('h3');
+titulo_teamwork.className = 'identity-values-value-title';
+let texto_titulo_teamwork = document.createTextNode('Trabajo en equipo');
+let parrafo_teamwork = document.createElement('p');
+parrafo_teamwork.className = 'identity-values-value-text';
+let texto_parrafo_teamwork = document.createTextNode('Somos un conjunto de personas comprometidas con nuestro rol para lograr un objetivo en común. (Personas, organización y objetivo común).');  
+/*  Se crea la estructura del artículo con appendChild  */
+teamwork.appendChild(imagen_teamwork);
+teamwork.appendChild(titulo_teamwork);
+titulo_teamwork.appendChild(texto_titulo_teamwork);
+teamwork.appendChild(parrafo_teamwork);
+parrafo_teamwork.appendChild(texto_parrafo_teamwork);
+/*  Se declaran las variables que contienen los abjetos HTML y sus contenidos
+    así como sus clases y rutas.     */
+let service = document.createElement('article');
+service.className = 'identity-values-value';
+let imagen_service = document.createElement('img');
+imagen_service.src = './assets/icons/serviceAttitude-icon.svg'
+imagen_service.className = 'identity-values-value-img';
+let titulo_service = document.createElement('h3');
+titulo_service.className = 'identity-values-value-title';
+let texto_titulo_service = document.createTextNode('Actitud de servicio');
+let parrafo_service = document.createElement('p');
+parrafo_service.className = 'identity-values-value-text';
+let texto_parrafo_service = document.createTextNode('Nos interesamos significativamente por los demás, donde todos salgan beneficiados.'); 
+/*  Se crea la estructura del artículo con appendChild  */
+service.appendChild(imagen_service);
+service.appendChild(titulo_service);
+titulo_service.appendChild(texto_titulo_service);
+service.appendChild(parrafo_service);
+parrafo_service.appendChild(texto_parrafo_service);
+/*  Se declaran las variables que contienen los abjetos HTML y sus contenidos
+    así como sus clases y rutas.     */
+let empathy = document.createElement('article');
+empathy.className = 'identity-values-value';
+let imagen_empathy = document.createElement('img');
+imagen_empathy.src = './assets/icons/empathy-icon.svg'
+imagen_empathy.className = 'identity-values-value-img';
+let titulo_empathy = document.createElement('h3');
+titulo_empathy.className = 'identity-values-value-title';
+let texto_titulo_empathy = document.createTextNode('Empatía');
+let parrafo_empathy = document.createElement('p');
+parrafo_empathy.className = 'identity-values-value-text';
+let texto_parrafo_empathy = document.createTextNode('Comprendemos, escuchamos activamente, desarrollamos la capacidad de actuar de manera solidaria y responsable para mejorar la calidad de vida de los residentes y de nuestros compañeros.'); 
+/*  Se crea la estructura del artículo con appendChild  */
+empathy.appendChild(imagen_empathy);
+empathy.appendChild(titulo_empathy);
+titulo_empathy.appendChild(texto_titulo_empathy);
+empathy.appendChild(parrafo_empathy);
+parrafo_empathy.appendChild(texto_parrafo_empathy);
+/*  Se declaran las variables que contienen los abjetos HTML y sus contenidos
+    así como sus clases y rutas.     */
+let ethic = document.createElement('article');
+ethic.className = 'identity-values-value';
+let imagen_ethic = document.createElement('img');
+imagen_ethic.src = './assets/icons/ethic-icon.svg'
+imagen_ethic.className = 'identity-values-value-img';
+let titulo_ethic = document.createElement('h3');
+titulo_ethic.className = 'identity-values-value-title';
+let texto_titulo_ethic = document.createTextNode('Ética');
+let parrafo_ethic = document.createElement('p');
+parrafo_ethic.className = 'identity-values-value-text';
+let texto_parrafo_ethic = document.createTextNode('Logramos diferenciar y destacar lo mejor de las personas, pues nos concentramos en los derechos y responsabilidades propios y de los demás, influyendo para solucionar conflictos con equidad.'); 
+/*  Se crea la estructura del artículo con appendChild  */
+ethic.appendChild(imagen_ethic);
+ethic.appendChild(titulo_ethic);
+titulo_ethic.appendChild(texto_titulo_ethic);
+ethic.appendChild(parrafo_ethic);
+parrafo_ethic.appendChild(texto_parrafo_ethic);
+/*  Se declaran las variables que contienen los abjetos HTML y sus contenidos
+    así como sus clases y rutas.     */
+let efficiency = document.createElement('article');
+efficiency.className = 'identity-values-value';
+let imagen_efficiency = document.createElement('img');
+imagen_efficiency.src = './assets/icons/efficiency-icon.svg'
+imagen_efficiency.className = 'identity-values-value-img';
+let titulo_efficiency = document.createElement('h3');
+titulo_efficiency.className = 'identity-values-value-title';
+let texto_titulo_efficiency = document.createTextNode('Eficiencia');
+let parrafo_efficiency = document.createElement('p');
+parrafo_efficiency.className = 'identity-values-value-text';
+let texto_parrafo_efficiency = document.createTextNode('Logramos los objetivos con la menor cantidad de recursos.'); 
+/*  Se crea la estructura del artículo con appendChild  */
+efficiency.appendChild(imagen_efficiency);
+efficiency.appendChild(titulo_efficiency);
+titulo_efficiency.appendChild(texto_titulo_efficiency);
+efficiency.appendChild(parrafo_efficiency);
+parrafo_efficiency.appendChild(texto_parrafo_efficiency);
+
+/*  Las siguientes funciones reemplazan el botón del valor que se selecciona por el valor desplegado 
+    y reemplazan el primer hijo de todos los demás valores por el botón de su valor correspondiente.    */
+
+/*  Hice todo lo siguiente, dado que había un error en el que al darle click al primer botón
+    se quedaba atorado el elemento hasta que se le diera click a otro botón y de regreso. 
+    No encontré como tal la raíz del error, pero repliqué la "solución" haciendo que la 
+    primera vez que se le diera click a un botón, me cargara el elemento, luego otro y de nuevo 
+    el primero, así logré que no se notara.                                                             */
+
+//  Variable donde se guarda la primera vez que se da click a cualquier botón.
+let first = 0;
+
+function ShowValueTeamwork () {
+//  Condicional que verifica si es la primera vez que se da el click
+    if (first == 0) {
+        SERVICE_CONTAIENER.replaceChild(service_button, SERVICE_CONTAIENER.firstChild);
+        EMPATHY_CONTAIENER.replaceChild(empathy_button, EMPATHY_CONTAIENER.firstChild);
+        ETHIC_CONTAIENER.replaceChild(ethic_button, ETHIC_CONTAIENER.firstChild);
+        EFFICIENCY_CONTAIENER.replaceChild(efficiency_button, EFFICIENCY_CONTAIENER.firstChild);
+        TEAMWORK_CONTAIENER.replaceChild(teamwork, teamwork_button);
+
+        TEAMWORK_CONTAIENER.replaceChild(teamwork_button, TEAMWORK_CONTAIENER.firstChild);
+        EMPATHY_CONTAIENER.replaceChild(empathy_button, EMPATHY_CONTAIENER.firstChild);
+        ETHIC_CONTAIENER.replaceChild(ethic_button, ETHIC_CONTAIENER.firstChild);
+        EFFICIENCY_CONTAIENER.replaceChild(efficiency_button, EFFICIENCY_CONTAIENER.firstChild);
+        SERVICE_CONTAIENER.replaceChild(service, service_button);
+
+        SERVICE_CONTAIENER.replaceChild(service_button, SERVICE_CONTAIENER.firstChild);
+        EMPATHY_CONTAIENER.replaceChild(empathy_button, EMPATHY_CONTAIENER.firstChild);
+        ETHIC_CONTAIENER.replaceChild(ethic_button, ETHIC_CONTAIENER.firstChild);
+        EFFICIENCY_CONTAIENER.replaceChild(efficiency_button, EFFICIENCY_CONTAIENER.firstChild);
+        TEAMWORK_CONTAIENER.replaceChild(teamwork, teamwork_button);
+//  Cambia el valor de first, indicando que ya se dio el primer click
+        first = 1;
+    }
+    else {
+        SERVICE_CONTAIENER.replaceChild(service_button, SERVICE_CONTAIENER.firstChild);
+        EMPATHY_CONTAIENER.replaceChild(empathy_button, EMPATHY_CONTAIENER.firstChild);
+        ETHIC_CONTAIENER.replaceChild(ethic_button, ETHIC_CONTAIENER.firstChild);
+        EFFICIENCY_CONTAIENER.replaceChild(efficiency_button, EFFICIENCY_CONTAIENER.firstChild);
+        TEAMWORK_CONTAIENER.replaceChild(teamwork, teamwork_button);
+    }
+    VALUES_CONTAINER.className = 'identity-values-container';
+}
+function ShowValueService () {
+    if (first == 0) {
+        TEAMWORK_CONTAIENER.replaceChild(teamwork_button, TEAMWORK_CONTAIENER.firstChild);
+        EMPATHY_CONTAIENER.replaceChild(empathy_button, EMPATHY_CONTAIENER.firstChild);
+        ETHIC_CONTAIENER.replaceChild(ethic_button, ETHIC_CONTAIENER.firstChild);
+        EFFICIENCY_CONTAIENER.replaceChild(efficiency_button, EFFICIENCY_CONTAIENER.firstChild);
+        SERVICE_CONTAIENER.replaceChild(service, service_button);
+
+        SERVICE_CONTAIENER.replaceChild(service_button, SERVICE_CONTAIENER.firstChild);
+        EMPATHY_CONTAIENER.replaceChild(empathy_button, EMPATHY_CONTAIENER.firstChild);
+        ETHIC_CONTAIENER.replaceChild(ethic_button, ETHIC_CONTAIENER.firstChild);
+        EFFICIENCY_CONTAIENER.replaceChild(efficiency_button, EFFICIENCY_CONTAIENER.firstChild);
+        TEAMWORK_CONTAIENER.replaceChild(teamwork, teamwork_button);
+
+        TEAMWORK_CONTAIENER.replaceChild(teamwork_button, TEAMWORK_CONTAIENER.firstChild);
+        EMPATHY_CONTAIENER.replaceChild(empathy_button, EMPATHY_CONTAIENER.firstChild);
+        ETHIC_CONTAIENER.replaceChild(ethic_button, ETHIC_CONTAIENER.firstChild);
+        EFFICIENCY_CONTAIENER.replaceChild(efficiency_button, EFFICIENCY_CONTAIENER.firstChild);
+        SERVICE_CONTAIENER.replaceChild(service, service_button)
+
+        first = 1;
+    }
+    else {
+        TEAMWORK_CONTAIENER.replaceChild(teamwork_button, TEAMWORK_CONTAIENER.firstChild);
+        EMPATHY_CONTAIENER.replaceChild(empathy_button, EMPATHY_CONTAIENER.firstChild);
+        ETHIC_CONTAIENER.replaceChild(ethic_button, ETHIC_CONTAIENER.firstChild);
+        EFFICIENCY_CONTAIENER.replaceChild(efficiency_button, EFFICIENCY_CONTAIENER.firstChild);
+        SERVICE_CONTAIENER.replaceChild(service, service_button);
+    }
+//  Se agrega una clase al contenedor al hacer click en el botón para que mantenga su tamaño.
+    VALUES_CONTAINER.className = 'identity-values-container';
+}
+function ShowValueEmpathy () {
+    if (first == 0) {
+        TEAMWORK_CONTAIENER.replaceChild(teamwork_button, TEAMWORK_CONTAIENER.firstChild);
+        SERVICE_CONTAIENER.replaceChild(service_button, SERVICE_CONTAIENER.firstChild);
+        ETHIC_CONTAIENER.replaceChild(ethic_button, ETHIC_CONTAIENER.firstChild);
+        EFFICIENCY_CONTAIENER.replaceChild(efficiency_button, EFFICIENCY_CONTAIENER.firstChild);
+        EMPATHY_CONTAIENER.replaceChild(empathy, empathy_button);
+
+        SERVICE_CONTAIENER.replaceChild(service_button, SERVICE_CONTAIENER.firstChild);
+        EMPATHY_CONTAIENER.replaceChild(empathy_button, EMPATHY_CONTAIENER.firstChild);
+        ETHIC_CONTAIENER.replaceChild(ethic_button, ETHIC_CONTAIENER.firstChild);
+        EFFICIENCY_CONTAIENER.replaceChild(efficiency_button, EFFICIENCY_CONTAIENER.firstChild);
+        TEAMWORK_CONTAIENER.replaceChild(teamwork, teamwork_button);
+
+        TEAMWORK_CONTAIENER.replaceChild(teamwork_button, TEAMWORK_CONTAIENER.firstChild);
+        SERVICE_CONTAIENER.replaceChild(service_button, SERVICE_CONTAIENER.firstChild);
+        ETHIC_CONTAIENER.replaceChild(ethic_button, ETHIC_CONTAIENER.firstChild);
+        EFFICIENCY_CONTAIENER.replaceChild(efficiency_button, EFFICIENCY_CONTAIENER.firstChild);
+        EMPATHY_CONTAIENER.replaceChild(empathy, empathy_button);
+
+        first = 1;
+    }
+    else {
+        TEAMWORK_CONTAIENER.replaceChild(teamwork_button, TEAMWORK_CONTAIENER.firstChild);
+        SERVICE_CONTAIENER.replaceChild(service_button, SERVICE_CONTAIENER.firstChild);
+        ETHIC_CONTAIENER.replaceChild(ethic_button, ETHIC_CONTAIENER.firstChild);
+        EFFICIENCY_CONTAIENER.replaceChild(efficiency_button, EFFICIENCY_CONTAIENER.firstChild);
+        EMPATHY_CONTAIENER.replaceChild(empathy, empathy_button);
+    }
+    VALUES_CONTAINER.className = 'identity-values-container';
+}
+function ShowValueEthic () {
+    if (first == 0) {
+        TEAMWORK_CONTAIENER.replaceChild(teamwork_button, TEAMWORK_CONTAIENER.firstChild);
+        SERVICE_CONTAIENER.replaceChild(service_button, SERVICE_CONTAIENER.firstChild);
+        EMPATHY_CONTAIENER.replaceChild(empathy_button, EMPATHY_CONTAIENER.firstChild);
+        EFFICIENCY_CONTAIENER.replaceChild(efficiency_button, EFFICIENCY_CONTAIENER.firstChild);
+        ETHIC_CONTAIENER.replaceChild(ethic, ethic_button);
+
+        SERVICE_CONTAIENER.replaceChild(service_button, SERVICE_CONTAIENER.firstChild);
+        EMPATHY_CONTAIENER.replaceChild(empathy_button, EMPATHY_CONTAIENER.firstChild);
+        ETHIC_CONTAIENER.replaceChild(ethic_button, ETHIC_CONTAIENER.firstChild);
+        EFFICIENCY_CONTAIENER.replaceChild(efficiency_button, EFFICIENCY_CONTAIENER.firstChild);
+        TEAMWORK_CONTAIENER.replaceChild(teamwork, teamwork_button);
+
+        TEAMWORK_CONTAIENER.replaceChild(teamwork_button, TEAMWORK_CONTAIENER.firstChild);
+        SERVICE_CONTAIENER.replaceChild(service_button, SERVICE_CONTAIENER.firstChild);
+        EMPATHY_CONTAIENER.replaceChild(empathy_button, EMPATHY_CONTAIENER.firstChild);
+        EFFICIENCY_CONTAIENER.replaceChild(efficiency_button, EFFICIENCY_CONTAIENER.firstChild);
+        ETHIC_CONTAIENER.replaceChild(ethic, ethic_button);
+
+        first = 1;
+    }
+    else {
+        TEAMWORK_CONTAIENER.replaceChild(teamwork_button, TEAMWORK_CONTAIENER.firstChild);
+        SERVICE_CONTAIENER.replaceChild(service_button, SERVICE_CONTAIENER.firstChild);
+        EMPATHY_CONTAIENER.replaceChild(empathy_button, EMPATHY_CONTAIENER.firstChild);
+        EFFICIENCY_CONTAIENER.replaceChild(efficiency_button, EFFICIENCY_CONTAIENER.firstChild);
+        ETHIC_CONTAIENER.replaceChild(ethic, ethic_button);
+    }
+    VALUES_CONTAINER.className = 'identity-values-container';
+}
+function ShowValueEfficiency () {
+    if (first == 0) {
+        TEAMWORK_CONTAIENER.replaceChild(teamwork_button, TEAMWORK_CONTAIENER.firstChild);
+        SERVICE_CONTAIENER.replaceChild(service_button, SERVICE_CONTAIENER.firstChild);
+        EMPATHY_CONTAIENER.replaceChild(empathy_button, EMPATHY_CONTAIENER.firstChild);
+        ETHIC_CONTAIENER.replaceChild(ethic_button, ETHIC_CONTAIENER.firstChild);
+        EFFICIENCY_CONTAIENER.replaceChild(efficiency, efficiency_button);
+
+        SERVICE_CONTAIENER.replaceChild(service_button, SERVICE_CONTAIENER.firstChild);
+        EMPATHY_CONTAIENER.replaceChild(empathy_button, EMPATHY_CONTAIENER.firstChild);
+        ETHIC_CONTAIENER.replaceChild(ethic_button, ETHIC_CONTAIENER.firstChild);
+        EFFICIENCY_CONTAIENER.replaceChild(efficiency_button, EFFICIENCY_CONTAIENER.firstChild);
+        TEAMWORK_CONTAIENER.replaceChild(teamwork, teamwork_button);
+
+        TEAMWORK_CONTAIENER.replaceChild(teamwork_button, TEAMWORK_CONTAIENER.firstChild);
+        SERVICE_CONTAIENER.replaceChild(service_button, SERVICE_CONTAIENER.firstChild);
+        EMPATHY_CONTAIENER.replaceChild(empathy_button, EMPATHY_CONTAIENER.firstChild);
+        ETHIC_CONTAIENER.replaceChild(ethic_button, ETHIC_CONTAIENER.firstChild);
+        EFFICIENCY_CONTAIENER.replaceChild(efficiency, efficiency_button);
+
+        first = 1;
+    }
+    else {
+        TEAMWORK_CONTAIENER.replaceChild(teamwork_button, TEAMWORK_CONTAIENER.firstChild);
+        SERVICE_CONTAIENER.replaceChild(service_button, SERVICE_CONTAIENER.firstChild);
+        EMPATHY_CONTAIENER.replaceChild(empathy_button, EMPATHY_CONTAIENER.firstChild);
+        ETHIC_CONTAIENER.replaceChild(ethic_button, ETHIC_CONTAIENER.firstChild);
+        EFFICIENCY_CONTAIENER.replaceChild(efficiency, efficiency_button);
+    }
+    VALUES_CONTAINER.className = 'identity-values-container';
+}
+
+function ReplaceButtons () {
+    TEAMWORK_CONTAIENER.replaceChild(teamwork_button, TEAMWORK_CONTAIENER.firstChild);
+    SERVICE_CONTAIENER.replaceChild(service_button, SERVICE_CONTAIENER.firstChild);
+    EMPATHY_CONTAIENER.replaceChild(empathy_button, EMPATHY_CONTAIENER.firstChild);
+    ETHIC_CONTAIENER.replaceChild(ethic_button, ETHIC_CONTAIENER.firstChild);
+    EFFICIENCY_CONTAIENER.replaceChild(efficiency_button, EFFICIENCY_CONTAIENER.firstChild);
+
+    VALUES_CONTAINER.className = '';
+}
+
---
+++ conócenos/staff.js
@@ -0,0 +1,185 @@
+const COLLABORATOR_IMAGE_1 = document.getElementById('collaborator-img-1');
+COLLABORATOR_IMAGE_1.addEventListener('click', changeClass1);
+const COLLABORATOR_IMAGE_2 = document.getElementById('collaborator-img-2');
+COLLABORATOR_IMAGE_2.addEventListener('click', changeClass2);
+const COLLABORATOR_IMAGE_3 = document.getElementById('collaborator-img-3');
+COLLABORATOR_IMAGE_3.addEventListener('click', changeClass3);
+const COLLABORATOR_IMAGE_4 = document.getElementById('collaborator-img-4');
+COLLABORATOR_IMAGE_4.addEventListener('click', changeClass4);
+const COLLABORATOR_IMAGE_5 = document.getElementById('collaborator-img-5');
+COLLABORATOR_IMAGE_5.addEventListener('click', changeClass5);
+const COLLABORATOR_IMAGE_6 = document.getElementById('collaborator-img-6');
+COLLABORATOR_IMAGE_6.addEventListener('click', changeClass6);
+const COLLABORATOR_IMAGE_7 = document.getElementById('collaborator-img-7');
+COLLABORATOR_IMAGE_7.addEventListener('click', changeClass7);
+const COLLABORATOR_IMAGE_8 = document.getElementById('collaborator-img-8');
+COLLABORATOR_IMAGE_8.addEventListener('click', changeClass8);
+const COLLABORATOR_IMAGE_9 = document.getElementById('collaborator-img-9');
+COLLABORATOR_IMAGE_9.addEventListener('click', changeClass9);
+const COLLABORATOR_IMAGE_10 = document.getElementById('collaborator-img-10');
+COLLABORATOR_IMAGE_10.addEventListener('click', changeClass10);
+const COLLABORATOR_IMAGE_11 = document.getElementById('collaborator-img-11');
+COLLABORATOR_IMAGE_11.addEventListener('click', changeClass11);
+const COLLABORATOR_IMAGE_12 = document.getElementById('collaborator-img-12');
+COLLABORATOR_IMAGE_12.addEventListener('click', changeClass12);
+const COLLABORATOR_IMAGE_13 = document.getElementById('collaborator-img-13');
+COLLABORATOR_IMAGE_13.addEventListener('click', changeClass13);
+const COLLABORATOR_IMAGE_14 = document.getElementById('collaborator-img-14');
+COLLABORATOR_IMAGE_14.addEventListener('click', changeClass14);
+
+
+let counter_1 = 0;
+let counter_2 = 0;
+let counter_3 = 0;
+let counter_4 = 0;
+let counter_5 = 0;
+let counter_6 = 0;
+let counter_7 = 0;
+let counter_8 = 0;
+let counter_9 = 0;
+let counter_10 = 0;
+let counter_11 = 0;
+let counter_12 = 0;
+let counter_13 = 0;
+let counter_14 = 0;
+
+function changeClass1 () {
+    if (counter_1 == 0) {
+        COLLABORATOR_IMAGE_1.className = 'staff-collaborator-container-img change1';
+        counter_1 = 1;
+    }
+    else {
+        COLLABORATOR_IMAGE_1.className = 'staff-collaborator-container-img change2';
+        counter_1 = 0;
+    }
+}
+function changeClass2 () {
+    if (counter_2 == 0) {
+        COLLABORATOR_IMAGE_2.className = 'staff-collaborator-container-img change1';
+        counter_2 = 1;
+    }
+    else {
+        COLLABORATOR_IMAGE_2.className = 'staff-collaborator-container-img change2';
+        counter_2 = 0;
+    }
+}
+function changeClass3 () {
+    if (counter_3 == 0) {
+        COLLABORATOR_IMAGE_3.className = 'staff-collaborator-container-img change1';
+        counter_3 = 1;
+    }
+    else {
+        COLLABORATOR_IMAGE_3.className = 'staff-collaborator-container-img change2';
+        counter_3 = 0;
+    }
+}
+function changeClass4 () {
+    if (counter_4 == 0) {
+        COLLABORATOR_IMAGE_4.className = 'staff-collaborator-container-img change1';
+        counter_4 = 1;
+    }
+    else {
+        COLLABORATOR_IMAGE_4.className = 'staff-collaborator-container-img change2';
+        counter_4 = 0;
+    }
+}
+function changeClass5 () {
+    if (counter_5 == 0) {
+        COLLABORATOR_IMAGE_5.className = 'staff-collaborator-container-img change1';
+        counter_5 = 1;
+    }
+    else {
+        COLLABORATOR_IMAGE_5.className = 'staff-collaborator-container-img change2';
+        counter_5 = 0;
+    }
+}
+function changeClass6 () {
+    if (counter_6 == 0) {
+        COLLABORATOR_IMAGE_6.className = 'staff-collaborator-container-img change1';
+        counter_6 = 1;
+    }
+    else {
+        COLLABORATOR_IMAGE_6.className = 'staff-collaborator-container-img change2';
+        counter_6 = 0;
+    }
+}
+function changeClass7 () {
+    if (counter_7 == 0) {
+        COLLABORATOR_IMAGE_7.className = 'staff-collaborator-container-img change1';
+        counter_7 = 1;
+    }
+    else {
+        COLLABORATOR_IMAGE_7.className = 'staff-collaborator-container-img change2';
+        counter_7 = 0;
+    }
+}
+function changeClass8 () {
+    if (counter_8 == 0) {
+        COLLABORATOR_IMAGE_8.className = 'staff-collaborator-container-img change1';
+        counter_8 = 1;
+    }
+    else {
+        COLLABORATOR_IMAGE_8.className = 'staff-collaborator-container-img change2';
+        counter_8 = 0;
+    }
+}
+function changeClass9 () {
+    if (counter_9 == 0) {
+        COLLABORATOR_IMAGE_9.className = 'staff-collaborator-container-img change1';
+        counter_9 = 1;
+    }
+    else {
+        COLLABORATOR_IMAGE_9.className = 'staff-collaborator-container-img change2';
+        counter_9 = 0;
+    }
+}
+    function changeClass10 () {
+    if (counter_10 == 0) {
+        COLLABORATOR_IMAGE_10.className = 'staff-collaborator-container-img change1';
+        counter_10 = 1;
+    }
+    else {
+        COLLABORATOR_IMAGE_10.className = 'staff-collaborator-container-img change2';
+        counter_10 = 0;
+    }
+}
+function changeClass11 () {
+    if (counter_11 == 0) {
+        COLLABORATOR_IMAGE_11.className = 'staff-collaborator-container-img change1';
+        counter_11 = 1;
+    }
+    else {
+        COLLABORATOR_IMAGE_11.className = 'staff-collaborator-container-img change2';
+        counter_11 = 0;
+    }
+}
+function changeClass12 () {
+    if (counter_12 == 0) {
+        COLLABORATOR_IMAGE_12.className = 'staff-collaborator-container-img change1';
+        counter_12 = 1;
+    }
+    else {
+        COLLABORATOR_IMAGE_12.className = 'staff-collaborator-container-img change2';
+        counter_12 = 0;
+    }
+}
+function changeClass13 () {
+    if (counter_13 == 0) {
+        COLLABORATOR_IMAGE_13.className = 'staff-collaborator-container-img change1';
+        counter_13 = 1;
+    }
+    else {
+        COLLABORATOR_IMAGE_13.className = 'staff-collaborator-container-img change2';
+        counter_13 = 0;
+    }
+}
+function changeClass14 () {
+    if (counter_14 == 0) {
+        COLLABORATOR_IMAGE_14.className = 'staff-collaborator-container-img change1';
+        counter_14 = 1;
+    }
+    else {
+        COLLABORATOR_IMAGE_14.className = 'staff-collaborator-container-img change2';
+        counter_14 = 0;
+    }
+}
